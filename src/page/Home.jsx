e868d64c5662b6210afe03dcfaa1e39dc49b0d37
天野実来 <miku@mamacbookpro.local>
2025-04-25 09:32:39 +0900

    fix:card layout

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -51,12 +51,12 @@ export default function Home() {
         <h2 className="p-3 bg-gradient-to-r from-purple-500 via-blue-500 to-purple-500 bg-clip-text text-6xl font-bold tracking-tight text-transparent mt-10" id="skills">
           Skills
         </h2>
-        <div className="w-100 max-w-6xl mx-auto mt-10 md:flex items-center md:justify-evenly md:columns-3 column-1">
-          <div class="my-2 md:w-[30%] max-w-sm mx-auto rounded overflow-hidden shadow-lg flip-up" data-aos="flip-up" data-aos-duration="500">
+        <div className="container mx-auto px-4 mt-10 flex items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div class="my-2 max-w-sm mx-auto rounded overflow-hidden shadow-md flip-up" data-aos="flip-up" data-aos-duration="500">
             <img class="w-2/3 mx-auto" src={Card1} alt="Sunset in the mountains"></img>
-            <div class="px-6 py-4">
+            <div class="py-4">
               <div class="font-bold text-xl mb-2">3DCG、ゲーム制作</div>
-              <p class="text-gray-700 text-base">
+              <p class="px-2 text-gray-700 text-base">
                 高校一年生の時にBlenderで3DCG制作を始める。その後Booth,Clusterで3DCGデータ販売を始め、収益化を達成。
                 Unityを使用してゲーム制作を行っている。
               </p>
@@ -67,11 +67,11 @@ export default function Home() {
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#winter</span>
             </div>
           </div>
-          <div class="my-2 md:w-[30%] mx-auto max-w-sm rounded overflow-hidden shadow-lg flip-up" data-aos="flip-up" data-aos-duration="1000">
+          <div class="my-2 mx-auto max-w-sm rounded overflow-hidden shadow-md flip-up" data-aos="flip-up" data-aos-duration="1000">
             <img class="w-2/3 mx-auto" src={Card2} alt="Sunset in the mountains"></img>
-            <div class="px-6 py-4">
+            <div class="py-4">
               <div class="font-bold text-xl mb-2">Webサイト制作</div>
-              <p class="text-gray-700 text-base">
+              <p class="px-2 text-gray-700 text-base">
                 高校1年生よりHTML/CSSでの開発を始める。<br></br>
                 Wordpress等のCMSを使用したWebサイト制作やGithubの共同開発を行い、現在はReact＋Viteでの開発経験を積んでいる。
               </p>
@@ -82,11 +82,11 @@ export default function Home() {
               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">#Wordpress</span>
             </div>
           </div>
-          <div class="my-2 md:w-[30%] mx-auto max-w-sm rounded overflow-hidden shadow-lg flip-up" data-aos="flip-up" data-aos-duration="1500">
+          <div class="my-2 mx-auto max-w-sm rounded overflow-hidden shadow-md flip-up" data-aos="flip-up" data-aos-duration="1500">
             <img class="w-2/3 mx-auto" src={Card3} alt="Sunset in the mountains"></img>
-            <div class="px-6 py-4">
+            <div class="py-4">
               <div class="font-bold text-xl mb-2">Webアプリケーション制作</div>
-              <p class="text-gray-700 text-base">
+              <p class="px-2 text-gray-700 text-base">
                 高校2年生よりReactAppを使用したWebアプリケーション制作を始める。
                 現在はSupabaseを使用したデータベース管理や、APIを使用したアプリケーション開発を行っている。
               </p>
